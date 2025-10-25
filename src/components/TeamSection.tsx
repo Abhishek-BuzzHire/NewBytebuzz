7c4a47b439fbed3a2a685962357bdb06f6cbdb1e
Abhishek-BuzzHire <abhishek.buzzhire@gmail.com>
2025-10-25 14:07:16 +0530

    25/10/2025 3rd commit

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -30,51 +30,51 @@ const Team = ({
         <div className="flex flex-wrap justify-center gap-8">
           {members.map((member) => (
             <div key={member.id} className="p-6">
-              <div className="group relative h-72 w-72 overflow-hidden rounded-lg bg-cover bg-center p-8 pt-4 transition-shadow hover:shadow-xl"
-
-                style={{
-                  backgroundImage: `url(/members/${member.image})`
-                }}>
-                {/* <div className="absolute inset-x-0 bottom-0 z-0 h-2/5 bg-gradient-to-t from-black/100 to-transparent" /> */}
-                <div className="relative z-10 flex h-full flex-col justify-end">
-                  <div className="details-container space-y-6 opacity-0 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0 translate-y-full">
-                    <div className="flex gap-3 justify-center">
-                      {member.github && (
-                        <a
-                          href={member.github} target="_blank"
-                          className="bg-primary rounded-lg p-2"
-                        >
-                          <Github className="text-white size-6" />
-                        </a>
-                      )}
-                      {member.twitter && (
-                        <a
-                          href={member.twitter} target="_blank"
-                          className="bg-primary rounded-lg p-2"
-                        >
-                          <Twitter className="text-white size-6" />
-                        </a>
-                      )}
-                      {member.linkedin && (
-                        <a
-                          href={member.linkedin} target="_blank"
-                          className="bg-primary rounded-lg p-2"
-                        >
-                          <Linkedin className="text-white size-6" />
-                        </a>
-                      )}
+              <a href={member.linkedin} target="_blank">
+                <div className="group relative h-72 w-72 overflow-hidden rounded-lg bg-cover bg-center p-8 pt-4 transition-shadow hover:shadow-xl"
+                  style={{
+                    backgroundImage: `url(/members/${member.image})`
+                  }}>
+                  {/* <div className="absolute inset-x-0 bottom-0 z-0 h-2/5 bg-gradient-to-t from-black/100 to-transparent" /> */}
+                  <div className="relative z-10 flex h-full flex-col justify-end">
+                    <div className="details-container space-y-6 opacity-0 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0 translate-y-full">
+                      <div className="flex gap-3 justify-center">
+                        {member.github && (
+                          <a
+                            href={member.github} target="_blank"
+                            className="bg-primary rounded-lg p-2"
+                          >
+                            <Github className="text-white size-6" />
+                          </a>
+                        )}
+                        {member.twitter && (
+                          <a
+                            href={member.twitter} target="_blank"
+                            className="bg-primary rounded-lg p-2"
+                          >
+                            <Twitter className="text-white size-6" />
+                          </a>
+                        )}
+                        {member.linkedin && (
+                          <div
+                            className="bg-primary rounded-lg p-2"
+                          >
+                            <Linkedin className="text-white size-6" />
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex justify-center mt-4">
-                    <div className="text-center text-foreground">
-                      <h3 className="mb-1 text-3xl font-semibold">{member.name}</h3>
-                      <p className="text-lg">
-                        {member.role}
-                      </p>
-                    </div>
+                <div className="flex justify-center mt-4">
+                  <div className="text-center text-foreground">
+                    <h3 className="mb-1 text-3xl font-semibold">{member.name}</h3>
+                    <p className="text-lg">
+                      {member.role}
+                    </p>
                   </div>
+                </div>
+              </a>
             </div>
           ))}
         </div>
